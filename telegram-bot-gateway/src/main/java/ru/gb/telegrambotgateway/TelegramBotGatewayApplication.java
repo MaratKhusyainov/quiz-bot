0808672eec760fc0ef0d99aff44587819651399c
Maxim <adm.mjkee@yandex.ru>
2021-07-26 15:42:43 +0300

    telegram-bot-gateway init

---
+++ telegram-bot-gateway/src/main/java/ru/gb/telegrambotgateway/TelegramBotGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.telegrambotgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotGatewayApplication.class, args);
+    }
+
+}
