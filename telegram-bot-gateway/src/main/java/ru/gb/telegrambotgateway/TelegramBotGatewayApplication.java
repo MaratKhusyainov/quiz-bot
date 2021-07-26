a70b9d90509bce0278da5e5729f42247b1cb74e7
Maxim <adm.mjkee@yandex.ru>
2021-07-26 18:23:32 +0300

    bot demo

--- telegram-bot-gateway/src/main/java/ru/gb/telegrambotgateway/TelegramBotGatewayApplication.java
+++ telegram-bot-gateway/src/main/java/ru/gb/telegrambotgateway/TelegramBotGatewayApplication.java
@@ -2,8 +2,10 @@ package ru.gb.telegrambotgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:telegram-bot-secret.properties")
 public class TelegramBotGatewayApplication {
 
     public static void main(String[] args) {
