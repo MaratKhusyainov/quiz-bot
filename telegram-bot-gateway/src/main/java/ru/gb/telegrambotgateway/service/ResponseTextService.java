0c9473006f0bbf01705bd2677345668f628d4b0a
Maxim <adm.mjkee@yandex.ru>
2021-08-19 19:49:46 +0300

    bot stages complete

--- telegram-bot-gateway/src/main/java/ru/gb/telegrambotgateway/service/ResponseTextService.java
+++ telegram-bot-gateway/src/main/java/ru/gb/telegrambotgateway/service/ResponseTextService.java
@@ -1,41 +1,14 @@
 package ru.gb.telegrambotgateway.service;
 
-public class ResponseTextService {
+public interface ResponseTextService {
 
-    public String getPlay(Long chatId) {
-        return  "Играем для " + chatId + System.lineSeparator() +
-            "Играем" + System.lineSeparator() +
-            "Играем";
-    }
+    String getDailyRating(Long chatId);
 
-    public String getDailyRating(Long chatId) {
-        return  "Рейтинг за день для " + chatId + System.lineSeparator() +
-                "Рейтинг за день" + System.lineSeparator() +
-                "Рейтинг за день";
-    }
+    String getWeeklyRating(Long chatId);
 
-    public String getWeeklyRating(Long chatId) {
-        return  "Рейтинг за неделю для " + chatId + System.lineSeparator() +
-                "Рейтинг за неделю" + System.lineSeparator() +
-                "Рейтинг за неделю";
-    }
+    String getMonthlyRating(Long chatId);
 
-    public String getMonthlyRating(Long chatId) {
-        return  "Рейтинг за месяц для " + chatId + System.lineSeparator() +
-                "Рейтинг за месяц" + System.lineSeparator() +
-                "Рейтинг за месяц";
-    }
+    String getTotalRating(Long chatId);
 
-    public String getTotalRating(Long chatId) {
-        return  "Общий рейтинг для " + chatId + System.lineSeparator() +
-                "Общий рейтинг" + System.lineSeparator() +
-                "Общий рейтинг";
-    }
-
-    public String getSupport() {
-        return  "Саппорт саппорт" + System.lineSeparator() +
-                "Саппорт саппорт" + System.lineSeparator() +
-                "Саппорт саппорт";
-    }
-
+    String getSupport();
 }
